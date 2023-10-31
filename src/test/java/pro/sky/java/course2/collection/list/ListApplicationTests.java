f2fce6cb7c57b8ed249b2d49a861f92fd15ec5cc
Solpostgit <begemot_off@mail.ru>
2023-10-31 22:55:30 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/collection/list/ListApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.collection.list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
